46a40f864335e6a1a487da82399753073e6811c4
PabloRSantos <pablorsantos15@gmail.com>
2020-09-17 00:47:44 -0300

    .

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
 import Routes from './src/routes'
 
-import ThemeProvider from './src/contexts/themes'
-
-import { AuthProvider } from './src/contexts/auth'
-
-import { ApolloProvider } from '@apollo/client'
-import client from './src/services/clientGraphql'
+import Contexts from './src/contexts/index'
 
 const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <AuthProvider>
-        <ThemeProvider>
-          <StatusBar barStyle="light-content" backgroundColor='#0098F6'/>
-          <Routes />
-        </ThemeProvider>
-      </AuthProvider>
-    </ApolloProvider>
+    <Contexts>
+      <Routes />
+    </Contexts>
 
   )
 }
